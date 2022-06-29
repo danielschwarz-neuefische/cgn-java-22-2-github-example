598f10e6f83882fc03d8ca5c5ccc46b593c743f7
Daniel Schwarz <daniel.schwarz@neuefische.de>
2022-06-29 11:32:19 +0200

    Improve text

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,6 +1,6 @@
 public class Greeting {
     public static void main(String[] args) {
         System.out.println("Hello Github!");
-        System.out.println("Welcome!");
+        System.out.println("Welcome everybody!");
     }
 }
